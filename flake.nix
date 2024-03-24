ddf78c59f976779544107219cc9c5b424043e86d
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-03-24 05:18:04 -0500

    nix: fix renovate

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The olsr.org Optimized Link-State Routing daemon (olsrd)";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   inputs.flake-utils.url = github:numtide/flake-utils;
 
   outputs = { self, nixpkgs, flake-utils }:
