b684846df90d0a988add5e099bbd24764cb4ce31
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-03-24 00:02:11 -0500

    make this a Nix flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "The olsr.org Optimized Link-State Routing daemon (olsrd)";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
+  inputs.flake-utils.url = github:numtide/flake-utils;
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        packages = rec {
+          default = pkgs.stdenv.mkDerivation rec {
+            name = "olsrd";
+            nativeBuildInputs = with pkgs; [
+              bison flex gnumake
+            ];
+            src = self;
+            buildPhase = "make";
+            installPhase = ''make install arprefresh_install txtinfo_install jsoninfo_install dot_draw_install watchdog_install nameservice_install PREFIX="$out" DESTDIR="$out"'';
+
+            meta = {
+              description = "The olsr.org Optimized Link-State Routing daemon (olsrd)";
+              license = pkgs.lib.licenses.bsd3;
+              platforms = pkgs.lib.platforms.all;
+              longDescription = ''
+                olsrd is an implementation of the Optimized Link State Routing protocol for Mobile Ad-Hoc networks(MANET). The protocol is described in RFC3626. It is designed to be run as a standalone server process.
+
+                OLSR operates as a table driven, proactive protocol, i.e., it exchanges topology information with other nodes of the network regularly. It is developed to work independently from other protocols. Likewise, OLSR makes no assumptions about the underlying link-layer. 
+              '';
+            };
+          };
+
+          olsrd = default;
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              bison flex gnumake
+            ];
+          };
+        };
+      }
+    );
+}
